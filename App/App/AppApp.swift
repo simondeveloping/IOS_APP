c93838b14550611a3d7f61573d3966aa5665b8d1
Boromir <simon.pham2005@gmail.com>
2026-05-29 10:00:09 +0200

    init projekt

---
+++ App/App/AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Boromir on 29.05.26.
+//
+
+import SwiftUI
+
+@main
+struct AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
